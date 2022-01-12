13ed7d118b8d87a3ba011f3bca2cb5cd32de3c94
yegorio228 <mr.melson1112@gmail.com>
2022-01-12 19:56:55 +0200

    Lesson30

---
+++ Lesson29/script.js
@@ -0,0 +1,25 @@
+// let x = 5
+// let y = 10
+let userName = "Yegor "
+let userAge = 15
+let isAdult = true
+let M_PI = 3.14
+
+// console.log(x+y)
+
+
+
+// userName = prompt("Как тебя зовут", "name")
+// userAge = prompt("Возраст", "0")
+// userSchool = prompt("Школа", "27")
+// console.log("\nМое имя: " + userName + "\nмой возраст: " + userAge + "\nСовершенолетний " + userSchool)
+
+let x = prompt("1число")
+let y = prompt("2число")
+
+// x = Number(x)
+// y = Number(y)
+
+
+
+console.log(Number(x)+Number(y))
---
+++ Lesson30/script.js
@@ -0,0 +1,108 @@
+// let namber = 1
+
+// console.log(namber+1)
+// console.log(typeof namber)
+
+// namber = 10n
+
+// console.log(namber)
+// console.log(typeof namber)
+
+// namber = "Строчка"
+
+// console.log(namber+1)
+// console.log(typeof namber)
+
+// console.log(10/namber)
+
+// let str = "Ничего"
+
+// let str1 = `Что
+// - ${1+3}
+// нибудь`
+
+// console.log(str1+str)
+
+// let isTrue = 4 > 1
+
+// console.log(isTrue)
+
+// let age = prompt()
+
+// console.log(age)
+
+// let age
+// console.log(age)
+
+// let number = 1
+// let str = "string"
+// console.log(typeof String(number))
+
+// number = number + 10
+// number = String(number)
+
+// console.log(typeof number)
+
+// let x = "6"
+// let y = "Привет"
+
+// console.log(x+y)
+// console.log(x/y)
+// console.log(x*y)
+
+// let x = 1
+// let y = 0
+
+// console.log(Boolean(x))
+// console.log(Boolean(y))
+
+// x = " "
+// y = `p`
+
+// console.log(Boolean(x))
+// console.log(Boolean(y))
+
+// x = NaN
+// y = null
+
+// console.log(Boolean(x))
+// console.log(Boolean(y))
+// console.log(Number(x))
+// console.log(Number(y))
+
+// let x = 5
+
+// console.log(typeof x)
+
+// let y = 10n
+
+// console.log(typeof y)
+
+// let q ="rfw"
+
+// console.log(typeof q)
+
+// let isTrue = 4 > 1
+
+// console.log(typeof isTrue)
+
+// let w = undefined
+
+// console.log(typeof w)
+
+// let e = null
+
+// console.log(typeof e)
+
+
+let h1 = document.querySelector(".h12")
+console.log(h1)
+
+function changeColor(){
+   let size = prompt("Введите розмер")
+   console.log(size)
+   h1.style.fontSize = size + `px`
+    // h1 = `34px`
+}
+
+// changeColor()
