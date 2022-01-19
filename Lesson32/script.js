44b4c90d206420e939d95d9984d30c748451a843
Yegorio228 <mr.melson1112@gmail.com>
2022-01-19 20:19:38 +0200

    Lesson32

---
+++ Lesson32/script.js
@@ -0,0 +1,72 @@
+// let z = 2
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(z *= 2)
+    
+// }
+
+// let Start = prompt('Start')
+// let Finish = prompt('Finish')
+
+// for (; Start <= Finish; Start++ ) {
+//     if( Start % 2 == 0)
+//     console.log(Start)
+// }
+
+// let  y = prompt('Start')
+
+// while(y == null){
+//      y = prompt('Start')
+// }
+// console.log(y)
+
+
+// do{
+//       y = prompt('Start')
+
+// }while(y == null)
+
+// console.log(y)
+
+// let w = prompt('Введите число')
+
+// while(isNaN(w)){
+//     w = prompt('Введите число')
+     
+// }
+// console.log(w)
+
+
+// let A = prompt('Введите число A')
+// let N = prompt('Введите число N')
+// let r = 0
+// for(let i = 0;N>=i; i++){
+//     r += A**i
+// }
+// console.log(r)
+
+
+// let N = prompt('Введите число N')
+// let r = 1
+// for(let i = 1;N>=i; i++){
+//     r *= i
+//     console.log(r)
+// }
+// console.log(r)
+
+
+// let A = prompt('Введите число A')
+// let N = prompt('Введите число N')
+// let r = 0
+// for(let i = 0;N>=i; i++){
+//     r += A**i
+// }
+// console.log(r)
+
+let N = prompt('Введите число N')
+let r = 0
+for(let i = 1;N>=i; i++){
+    r = i**i
+    console.log(r)
+}
+console.log(r)
